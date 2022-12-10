c63fc9f8436e4ed38c9dc9ee6fc7081a023ae033
Inna Kirianova <innr@ya.ru>
2022-12-11 02:00:40 +0300

    changed code for task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -6,7 +6,7 @@ int[] FillArray(int size)
     int[] arr = new int[size];
     for(int i = 0; i < arr.Length; i++)
     {
-        Console.Write("Введите число: ");
+        Console.Write($"Введите число {i+1}: ");
         arr[i] = Convert.ToInt32(Console.ReadLine());
     }
     return arr;
