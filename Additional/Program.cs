4ad5dd397995678b2ae996db10b65e8568e99789
Inna Kirianova <innr@ya.ru>
2022-12-12 01:25:35 +0300

    added code for additional task

---
+++ Additional/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+void Total(int number)
+{
+    int count = 0;
+    if (number == 0)
+    {
+        return;
+    }
+    else
+    {
+        Total(number / 10);
+        count += 1;
+    }
+    Console.Write(count);
+
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Total(num);
